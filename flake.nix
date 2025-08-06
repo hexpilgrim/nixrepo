efb5b079be79f0eef9ea6e2a6abf0f20e5b767a0
hexpilgrim <hexpilgrim@proton.me>
2025-08-06 09:51:51 +0100

    ci(nix): add CI workflow for package validation

--- flake.nix
+++ flake.nix
@@ -5,15 +5,24 @@
   inputs.nixpkgs.url = "github:NixOS/nixpkgs";
 
   outputs = { self, nixpkgs }:
-  let
-    system = "x86_64-linux";
-    pkgs = import nixpkgs {
-      system = system;
-      config = {
-        allowUnfree = true;
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs {
+        system = system;
+        config.allowUnfree = true;
       };
+
+      packageDirs = builtins.filter (name:
+        builtins.pathExists (./pkgs + "/${name}/default.nix")
+      ) (builtins.attrNames (builtins.readDir ./pkgs));
+
+      allPackages = builtins.listToAttrs (
+        map (name: {
+          name = name;
+          value = pkgs.callPackage (./pkgs + "/${name}/default.nix") {};
+        }) packageDirs
+      );
+    in {
+      packages.${system} = allPackages;
     };
-  in {
-    packages.${system}.cursor = pkgs.callPackage ./pkgs/cursor/default.nix {};
-  };
 }
